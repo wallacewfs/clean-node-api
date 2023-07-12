522b592d69dc80d03e38db0939b82668ebaf2451
Wallace Ferreira <wallacef@gmail.com>
2023-07-12 15:43:09 -0300

    feat: ensurte mongodb reconnect if client is down

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -5,7 +5,7 @@ import { MongoHelper } from '../helpers/mongo-helpers'
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
-    const accountCollection = MongoHelper.getColletion('accounts')
+    const accountCollection = await MongoHelper.getColletion('accounts')
     const result = await accountCollection.insertOne(accountData)
     const accountInserted = await accountCollection.findOne({ _id: result.insertedId })
     return MongoHelper.map(accountInserted)
