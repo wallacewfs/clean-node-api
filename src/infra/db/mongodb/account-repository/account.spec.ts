eb5103c542ed1f877fa70e27bcf2442b57817162
Wallace Ferreira <wallacef@gmail.com>
2023-07-19 19:11:26 -0300

    test: ensure AccountMongoRepository returns an loadbyemail succes

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,5 +1,8 @@
+import { Collection } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helpers'
 import { AccountMongoRepository } from './account'
+
+let accountCollection: Collection
 
 describe('Account Mogo Repository', () => {
   beforeAll(async () => {
@@ -10,7 +13,7 @@ describe('Account Mogo Repository', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getColletion('accounts')
+    accountCollection = await MongoHelper.getColletion('accounts')
     await accountCollection.deleteMany({})
   }
   )
@@ -18,7 +21,7 @@ describe('Account Mogo Repository', () => {
   const makeSut = (): AccountMongoRepository => {
     return new AccountMongoRepository()
   }
-  test('Should return an account on sucess', async () => {
+  test('Should return an account on add sucess', async () => {
     const sut = makeSut()
     const account = await sut.add({
       name: 'any_name',
@@ -30,5 +33,26 @@ describe('Account Mogo Repository', () => {
     expect(account.name).toBe('any_name')
     expect(account.email).toBe('any_email@mail.com')
     expect(account.password).toBe('any_password')
+  })
+
+  test('Should return an account on loadByEmail sucess', async () => {
+    const sut = makeSut()
+    await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    const account = await sut.loadbyEmail('any_email@mail.com')
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+
+  test('Should return null if loadByEmail fails', async () => {
+    const sut = makeSut()
+    const account = await sut.loadbyEmail('any_email@mail.com')
+    expect(account).toBeFalsy()
   })
 })
