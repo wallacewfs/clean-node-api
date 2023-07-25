faccb8957b7af1f6829921d90125688fd540e84f
Wallace Ferreira <wallacef@gmail.com>
2023-07-25 15:50:23 -0300

    chore: enable debug with vscode

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -4,10 +4,9 @@ import { readdirSync } from 'fs'
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  // fg.sync('**/src/main/routes/**routes.ts').map(async file => (await import(`../../../${file}`)).default(router))
   // eslint-disable-next-line n/no-path-concat
   readdirSync(`${__dirname}/../routes`).map(async file => {
-    if (!file.includes('.test.')) {
+    if (!file.includes('.test.') && !file.endsWith('.map')) {
       (await import(`../routes/${file}`)).default(router)
     }
   })
