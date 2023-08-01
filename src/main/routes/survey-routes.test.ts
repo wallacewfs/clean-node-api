aa89e355b960e5318bfaa9515c215bb75152cf7b
Wallace Ferreira <wallacef@gmail.com>
2023-08-01 10:58:06 -0300

    feat: add AddSurvey composition

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,38 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helpers'
+import { Collection } from 'mongodb'
+
+let surveytCollection: Collection
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveytCollection = await MongoHelper.getColletion('surveys')
+    await surveytCollection.deleteMany({})
+  }
+  )
+
+  describe('POST /surveys', () => {
+    test('Should return 204 on add survey success', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          }, {
+            answer: 'Ansewer 2'
+          }]
+        })
+        .expect(204)
+    })
+  })
+})
