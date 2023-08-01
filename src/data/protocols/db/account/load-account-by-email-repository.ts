d91eaa6a309ad2894e02ffcea94bc523f289b01f
Wallace Ferreira <wallacef@gmail.com>
2023-08-01 19:16:23 -0300

    feat: ensure DbLoadAccountByToken calls LoadAccountByTokenRepository with correct values

--- src/data/protocols/db/account/load-account-by-email-repository.ts
+++ src/data/protocols/db/account/load-account-by-email-repository.ts
@@ -1,5 +1,5 @@
 import { AccountModel } from '../../../../domain/models/account'
 
 export interface LoadAccountByEmailRepository {
-  loadbyEmail (email: string): Promise<AccountModel>
+  loadbyEmail (email: string): Promise<AccountModel | null>
 }
