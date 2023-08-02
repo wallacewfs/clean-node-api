d173deb21bd92303a9282ef9f3269225a7a2da9e
Wallace Ferreira <wallacef@gmail.com>
2023-08-02 16:33:16 -0300

    test: ensure AccountMongoRepository will not find an account if find account by token fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -111,5 +111,11 @@ describe('Account Mogo Repository', () => {
       expect(account?.email).toBe('any_email@mail.com')
       expect(account?.password).toBe('any_password')
     })
+
+    test('Should return null if loadByEmail fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeFalsy()
+    })
   })
 })
