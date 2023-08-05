621a2bcc5c4a92572c80f7669c30c9d6e11b822f
Wallace Ferreira <wallacef@gmail.com>
2023-08-05 19:03:20 -0300

    refactor: change models to use omit

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -10,9 +10,7 @@ export const adaptRoute = (controller: Controller) => {
     if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
       res.status(httpResponse.statusCode).json(httpResponse.body)
     } else {
-      res.status(httpResponse.statusCode).json({
-        error: httpResponse.body.message
-      })
+      res.status(httpResponse.statusCode).json({ error: httpResponse.body.message })
     }
   }
 }
