6728e4d6c8640db5fed94f06d90984c70d2e043f
Wallace Ferreira <wallacef@gmail.com>
2023-08-05 17:52:13 -0300

    refactor: change models to use type alias instead of interfaces

--- src/presentation/controllers/login/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login/login-controller.spec.ts
@@ -21,7 +21,7 @@ const makeAuthentication = (): Authentication => {
   return new AuthenticationStub()
 }
 
-interface SutTypes {
+type SutTypes = {
   sut: LoginController
   authenticationStub: Authentication
   validationStub: Validation
--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -25,7 +25,7 @@ const makeLoadAccountbyToken = (): LoadAccountByToken => {
   return new LoadAccountByTokenStub()
 }
 
-interface SutTypes {
+type SutTypes = {
   sut: AuthMiddleware
   loadAccountByTokenStub: LoadAccountByToken
 }
