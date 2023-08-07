660307f8294030649d15fcf5b9132a53bdebe138
Wallace Ferreira <wallacef@gmail.com>
2023-08-07 15:28:08 -0300

    feat: ensure DbLoadSurveyById call LoadSurveyByIdRepository with correct id

--- src/domain/usecases/load-surveys-by-id.ts
+++ src/domain/usecases/load-surveys-by-id.ts
@@ -1,5 +1,5 @@
 import { SurveyModel } from '@/domain/models/survey'
 
 export interface LoadSurveyById {
-  loadById (): Promise<SurveyModel | null>
+  loadById (id: string): Promise<SurveyModel | null>
 }
