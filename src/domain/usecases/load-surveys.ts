064a42aa9375a15f7b53f2b22434a1898313706d
wallacewfs <wallacef@gmail.com>
2023-12-22 23:48:43 -0300

    refactor: add types to LoadSurveys usecase

--- src/domain/usecases/load-surveys.ts
+++ src/domain/usecases/load-surveys.ts
@@ -1,5 +1,9 @@
 import { SurveyModel } from '@/domain/models/survey'
 
 export interface LoadSurveys {
-  load (accoundId: string): Promise<SurveyModel[] | null>
+  load (accoundId: string): Promise<LoadSurveys.Result | null>
+}
+
+export namespace LoadSurveys {
+  export type Result = SurveyModel[]
 }
