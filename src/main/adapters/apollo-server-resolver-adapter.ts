938af4681c613508f617b4d215ffb9c9c35e978c
wallacewfs <wallacef@gmail.com>
2023-12-27 01:55:18 -0300

    feat: add status code error on exceptions

--- src/main/adapters/apollo-server-resolver-adapter.ts
+++ src/main/adapters/apollo-server-resolver-adapter.ts
@@ -1,6 +1,14 @@
 import { Controller } from '@/presentation/protocols'
+import { UserInputError, AuthenticationError, ForbiddenError, ApolloError } from 'apollo-server-express'
 
 export const adaptResolver = async (controller: Controller, args: any): Promise<any> => {
   const httResponse = await controller.handle(args)
-  return httResponse.body
+  switch (httResponse.statusCode) {
+    case 200:
+    case 204: return httResponse.body
+    case 400: throw new UserInputError(httResponse.body.message)
+    case 401: throw new AuthenticationError(httResponse.body.message)
+    case 403: throw new ForbiddenError(httResponse.body.message)
+    default: throw new ApolloError(httResponse.body.message)
+  }
 }
--- src/main/graphql/apollo/apollo-server.ts
+++ src/main/graphql/apollo/apollo-server.ts
@@ -1,10 +1,36 @@
 import typeDefs from '@/main/graphql/type-Defs'
 import resolvers from '@/main/graphql/resolvers'
+
 import { ApolloServer } from 'apollo-server-express'
+import { GraphQLError } from 'graphql'
+
+const handleErrors = (response: any, errors: readonly GraphQLError[]): void => {
+  errors?.forEach(error => {
+    response.data = undefined
+    if (checkError(error, 'UserInputError')) {
+      response.http.status = 400
+    } else if (checkError(error, 'AuthenticationError')) {
+      response.http.status = 401
+    } else if (checkError(error, 'ForbiddenError')) {
+      response.http.status = 403
+    } else {
+      response.http.status = 500
+    }
+  })
+}
+
+const checkError = (error: GraphQLError, errorName: string): boolean => {
+  return [error.name, error.originalError?.name].some(name => name === errorName)
+}
 
 export const setupApolloServer = (): ApolloServer => {
   return new ApolloServer({
     resolvers,
-    typeDefs
+    typeDefs,
+    plugins: [{
+      requestDidStart: async () => ({
+        willSendResponse: async ({ response, errors }) => handleErrors(response, errors)
+      })
+    }]
   })
 }
